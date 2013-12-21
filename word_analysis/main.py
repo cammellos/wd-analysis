bd5d64d0a2663954a5edcb07e90a08edf755d232
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2013-12-21 11:32:00 +0000

    project structure & config

---
+++ word_analysis/main.py
@@ -0,0 +1,11 @@
+import word_analysis.config.config as config
+
+class Main(object):
+  def start(self):
+    config.config()
+    
+   
+
+if __name__ == '__main__':
+  main = Main()
+  main.start()
